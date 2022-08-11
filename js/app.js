ee0cc1e1369a485b3e2a1d50381ee5a0d5d05f8a
willie m <zx10rwillie@gmail.com>
2022-08-10 19:20:34 -0500

    updating js
    got API working

--- js/app.js
+++ js/app.js
@@ -10,3 +10,26 @@ fetch(urlAPI)
 .then(res => res.results)
 .then(displayEmployees)
 .catch(err => console.log(err));
+
+function displayEmployees (employeeData) {
+  employees = employeeData;
+    let employeeHTML = '';
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+  employeeHTML += `
+      <div class="card-container" data-index="${index}">
+      <img class="profile-pic" src="${picture.large}" />
+      <div class="text-container">
+      <h2 class="name">${name.first} ${name.last}</h2>
+      <a class="email">${email}</a>
+      <p class="address">${city}</p>
+      </div>
+      </div>
+      `
+  });
+  gridContainer.innerHTML = employeeHTML;
+
+}
